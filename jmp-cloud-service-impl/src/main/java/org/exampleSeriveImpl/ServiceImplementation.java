6940aee332e14fc009f40fe92bde934be0887684
Dziyana Turomsha <dziyana_turomsha@epam.com>
2023-04-21 15:08:33 +0200

    Service Interface update+Service Implementation update

--- jmp-cloud-service-impl/src/main/java/org/exampleSeriveImpl/ServiceImplementation.java
+++ jmp-cloud-service-impl/src/main/java/org/exampleSeriveImpl/ServiceImplementation.java
@@ -3,21 +3,18 @@ package org.exampleSeriveImpl;
 import org.ServiceAPI.Service;
 import org.example.User;
 
-import java.util.*;
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.Optional;
 
 public class ServiceImplementation implements Service {
     public ServiceImplementation(Optional<Object> nameUser) {
         this.nameUser = nameUser;
     }
 
+    public Optional<Object> nameUser;
+
+
     public List<User> getAllUsers() {
         return null;
     }
-
-    public Optional<Object> nameUser;
-
-    String[] userNames = {"name1", "name2"};
-    List<String> nameList = new ArrayList<>(List.of(userNames));
-    Stream<String> nameStream = Arrays.stream(userNames);
 }
